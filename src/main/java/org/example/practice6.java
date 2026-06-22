b7527278d2db98583674635b091c4713572bd0cc
Mihir Patel <163976907+mihirpatel03200@users.noreply.github.com>
2026-06-22 12:32:43 +0530

    Practice6

---
+++ src/main/java/org/example/practice6.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.List;
+
+public class practice6 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 500)");
+        js.executeScript("document.querySelector('.tableFixHead').scrollTop=500");
+
+        List < WebElement > values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+
+        int sum = 0;
+        for (int i = 0; i < values.size(); i++) {
+            sum = sum  + Integer.parseInt(values.get(i).getText());
+        }
+
+        System.out.println("Total sum: " +sum);
+        int amount = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
+        Assert.assertEquals(sum, amount);
+
+        WebElement table = driver.findElement(By.id("product"));
+        System.out.println(table.findElements(By.tagName("tr")).size());
+
+
+
+
+
+    }
+}
